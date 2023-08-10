3553ace3affc9428a5460f8a7afe56f511cc4257
tizzle <till@eightdaysaweek.cc>
2023-08-10 12:51:10 +0200

    fix: prevent touchmove propagation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import AppBar from "./components/app-bar";
 import FilterBar from "./components/filter-bar";
 import SuggestionsSheet from "./components/sheet";
@@ -5,7 +6,25 @@ import StoreBar from "./components/store-bar";
 import TopBar from "./components/top-bar";
 import ZeroSearch from "./components/zero-search";
 
+const stopReloadTouch = true;
+
 const App = () => {
+  React.useEffect(() => {
+    console.log("startup effect");
+    window.addEventListener(
+      "touchmove",
+      (ev) => {
+        console.log("touch move");
+        if (stopReloadTouch) {
+          console.log("stopped propagation");
+          ev.preventDefault();
+          ev.stopImmediatePropagation();
+        }
+      },
+      { passive: false }
+    );
+  }, []);
+
   return (
     <div className="flex flex-col h-full">
       <TopBar />
